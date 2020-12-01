b6461329b3c515dfda682d26802ed8b39ceb7456
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-01 17:32:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,18 +2,18 @@ def introduction name
   puts "hi, my name is #{name}"
 end
 
-introduction
-
 def introduction_with_language name language
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language
+
 
 def introduction_with_language_optional name language = "Ruby"
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
+introduction
+introduction_with_language
 introduction_with_language_optional
 
 
